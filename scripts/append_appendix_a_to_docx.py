7c67bac9e30bfb6a1a1cc5140801f4cf9896d21f
Anurag Yadav <ay468@cornell.edu>
2026-05-18 16:13:12 -0400

    Delete scripts directory

--- scripts/append_appendix_a_to_docx.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-"""Rebuild Appendix A in the thesis DOCX (delegates to format_docx_appendix.py)."""
-from format_docx_appendix import main
-
-if __name__ == "__main__":
-    main()
